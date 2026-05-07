ffa225a65672aea125337ed3ed7f198a969abec6
estebanvalenccia-coder <estebanvalenccia@gmail.com>
2026-05-07 23:18:57 +0200

    Refactor App component to use RouterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-import { Flower2 } from "lucide-react";
-import { Routes, Route, Link } from "react-router-dom";
-import { AdminInventory } from "./AdminInventory";
-import { ClientBouquetAI } from "./ClientBouquetAI";
+import React from "react";
+import { RouterProvider } from "react-router";
+import { router } from "./app/routes";
 
-export function App() {
-  return (
-    <>
-      <header className="topbar dark-topbar">
-        <div className="logo">
-          <Flower2 />
-          <span>FLORES</span>
-        </div>
-
-        <nav>
-          <Link to="/admin">Admin</Link>
-          <Link to="/clientes">Clientes IA</Link>
-        </nav>
-      </header>
-
-      <Routes>
-        <Route path="/admin" element={<AdminInventory />} />
-        <Route path="/clientes" element={<ClientBouquetAI />} />
-        <Route path="*" element={<ClientBouquetAI />} />
-      </Routes>
-    </>
-  );
+export default function App() {
+  return <RouterProvider router={router} />;
 }
